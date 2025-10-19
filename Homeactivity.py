b183a151f7f0432181d3f4c566980aa2010a583b
youngsheldonstaff-sketch <youngsheldonstaff@gmail.com>
2025-10-19 10:48:01 +0300

    Homework

---
+++ Homeactivity.py
@@ -0,0 +1,13 @@
+#printing Birth dates
+
+
+print ("these are my friends birthdays")
+
+
+birth1= "1st of march 2013"
+birth2= "11th of october 2013"
+birth3= "2.9,2013"
+birth4="10_3_2012"
+
+
+print (birth1,birth2,birth3,birth4)
